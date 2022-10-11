0e20f16e10ad70e69bf77ab0fe8835e4a9f0b3eb
Bhartendu Kumar <bhartendukr33@gmail.com>
2022-10-11 22:17:24 +0530

    no message

--- app/src/main/java/com/app/ecriture/ProfileFragment.kt
+++ app/src/main/java/com/app/ecriture/ProfileFragment.kt
@@ -4,9 +4,9 @@ import com.app.ecriture.base.BaseFragment
 import com.app.ecriture.databinding.FragmentProfileBinding
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
     override fun initView() {
-
+        TODO("Not yet implemented")
     }
-
 
 }
